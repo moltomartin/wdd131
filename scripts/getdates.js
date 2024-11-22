5e7b952fc25edd35de7065153c3108938e93e751
martinmolto <mol20009@byui.edu>
2024-11-22 02:42:40 -0300

    final

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const currentyear = document.querySelector("#currentyear");
+
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+currentyear.innerHTML = today.getFullYear();
+
+lastModified.innerHTML = document.lastModified;
